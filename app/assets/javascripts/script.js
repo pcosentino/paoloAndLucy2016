6dfee99483440f30bdac4baa348339cf00f20f3a
Paolo Cosentino <cosepaol88@gmail.com>
2016-01-09 18:13:01 -0600

    fix nav scroll location

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -54,7 +54,7 @@ function scrollNav() {
     $('.'+theClass).parent('li').addClass('active-link');
     //Animate
     $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 83
+        scrollTop: $( $(this).attr('href') ).offset().top - 50
     }, 400);
     return false;
 
