d5e2dc4a3358a3abf16953c39cd7bd605c62b851
Paolo Cosentino <cosepaol88@gmail.com>
2016-02-11 20:38:27 -0600

    update pics

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PaoloAndLucy2016
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.otf)
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
